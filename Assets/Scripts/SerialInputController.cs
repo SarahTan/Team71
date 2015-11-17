bb84415cf9e68fd1461c105820f5807b8374cefb
Sarah <sarahtan@cmu.edu>
2015-11-17 15:27:04 -0500

    Added Unity project

---
+++ Assets/Scripts/SerialInputController.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System;
+public class SerialInputController : MonoBehaviour {
+	public SoundManager soundMgr;
+	private SerialPort sp=new SerialPort("/dev/cu.usbmodem1411",9600);
+	char[] byteArray;
+	public bool[] isToushed;
+	int [] touchBuffer;
+	int buffersize=2;
+	// Use this for initialization
+	void Start () {
+
+		byteArray=new char[3];
+		isToushed=new bool[18];
+		touchBuffer = new int[18];
+		for (int i=0; i<18; i++) {
+			touchBuffer[i]=0;
+			isToushed[i]=false;
+		}
+
+			sp.Open ();	
+			sp.ReadTimeout = 20;
+
+		
+	}
+
+	void OnApplicationQuit(){
+		sp.Close ();
+	}
+	void updateData(bool[] newData){
+		//Debug.Log (newData.Length);
+
+		//18input
+		for (int i=0; i<18; i++) {
+			if(newData[i]!=isToushed[i]){
+				touchBuffer[i]++;
+				if(touchBuffer[i]>=buffersize){
+					touchBuffer[i]=0;
+					isToushed[i]=newData[i];
+				}
+			}else{
+				touchBuffer[i]=0;			}
+		}
+
+		//debug
+		string dbg="";
+		for (int i=0; i<18; i++) {
+			if(isToushed[i]){
+				soundMgr.SendInput(i);
+			}
+			dbg+=(isToushed[i]?'1':'0');
+		}
+		Debug.Log (dbg);
+
+	}
+	void touchController(string[] s){
+
+
+		byte[] data = new byte[3];
+		for (int i=0; i<3; i++) {
+
+
+
+			data[i]=Convert.ToByte(s[i],16);
+			
+		}
+		//convert 3 byte data to 18 bool
+		bool[] status=new bool[18];
+		for(int j=0;j<2;j++)
+		for (int i=0; i<8; i++) {
+		status[j*8+i]=((data[j]>>i)&0x01)==1?true:false;
+		}
+		status [16] = ((data [2] >> 0) & 0x01) == 1 ? true : false;
+		status [17] = ((data [2] >> 1) & 0x01) == 1 ? true : false;
+
+		updateData (status);
+		//debug
+		/*
+		 * string dbg="";
+		for (int i=0; i<18; i++) {
+			dbg+=(status[i]?'1':'0');
+		}
+				Debug.Log (dbg);
+		 * */
+
+	}
+	
+	
+	void FixedUpdate () {
+		if (!sp.IsOpen) {
+			sp.Open ();
+		}
+
+			if (sp != null && sp.IsOpen) {
+			try {
+				string received=sp.ReadLine();
+				//Debug.Log(received);
+				string[] words=sp.ReadLine().Split('\t','\r');
+				//Debug.Log(sp.ReadLine());
+
+
+				if(words.Length==4)touchController(words);//the last word is return
+			} catch (System.Exception) {
+				
+			}
+			sp.Close();
+			
+		} else {
+				sp.Open();
+		}
+		
+		
+	}
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	int numTracks = 6;
+	Transform tracksParent;
+	AudioSource feedback;
+	AudioSource[] tracks;
+	float[] tracksVol;
+
+	// Use this for initialization
+	void Start () {
+		feedback = transform.FindChild ("Feedback").GetComponent<AudioSource> ();
+		tracksParent = transform.FindChild ("Tracks");
+
+		tracks = new AudioSource[numTracks];
+		for (int i = 0; i < numTracks; i++) {
+			tracks[i] = tracksParent.GetChild(i).GetComponent<AudioSource>();
+		}
+
+		// Set the play volume for each of the tracks
+		tracksVol = new float[numTracks];
+		tracksVol [0] = tracksVol [1] = tracksVol [2] = tracksVol [4] = 1f;
+		tracksVol [3] = 0.75f;
+		tracksVol [5] = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Using W, A, S, D, F, G
+		if (Input.GetKeyDown ("w")) {
+			StartCoroutine (IncreaseVol (0));
+
+		} else if (Input.GetKeyDown ("a")) {
+			StartCoroutine (IncreaseVol (1));
+			
+		} else if (Input.GetKeyDown ("s")) {
+			StartCoroutine (IncreaseVol (2));
+			
+		} else if (Input.GetKeyDown ("d")) {
+			StartCoroutine (IncreaseVol (3));
+			
+		} else if (Input.GetKeyDown ("f")) {
+			StartCoroutine (IncreaseVol (4));
+			
+		} else if (Input.GetKeyDown ("g")) {
+			StartCoroutine (IncreaseVol (5));			
+		}
+	}
+
+	// CALL THIS FUNCTION
+	public void SendInput (int track) {
+		switch (track) {
+		case 11:
+			StartCoroutine (IncreaseVol (0));
+			break;
+
+		case 10:
+			StartCoroutine (IncreaseVol (1));
+			break;
+
+		case 9:
+			StartCoroutine (IncreaseVol (2));
+			break;
+			
+		case 8:
+			StartCoroutine (IncreaseVol (3));
+			break;
+		case 17:
+			StartCoroutine (IncreaseVol (4));
+			break;
+			
+		case 16:
+			StartCoroutine (IncreaseVol (5));
+			break;
+
+		default:
+			break;
+		}
+	}
+
+
+	IEnumerator IncreaseVol (int trackNum) {
+		feedback.Play();
+
+		if (tracks [trackNum].volume == 0f) {
+			Debug.Log ("Track " + trackNum + " starting");
+			while (tracks[trackNum].volume < tracksVol[trackNum]) {
+				tracks [trackNum].volume += 0.05f;
+				yield return null;
+			}
+			StartCoroutine (DecreaseVol (trackNum));
+		} else {
+			Debug.Log ("Track " + trackNum + " already playing");
+		}
+
+	}
+
+	IEnumerator DecreaseVol (int trackNum) {
+		yield return new WaitForSeconds (8f);
+		Debug.Log ("Track " + trackNum + " ending");
+
+		while (tracks[trackNum].volume > 0) {
+			tracks [trackNum].volume -= 0.05f;
+			yield return null;
+		}
+	}
+
+}
